58863243e52fe524a71861ab5780d688c68da683
blessed <cj.fullstack.work@gmail.com>
2026-02-01 16:02:16 -0500

    -Change branch to master

--- boot.sh
+++ boot.sh
@@ -5,11 +5,11 @@ export AURA_ONLINE_INSTALL=true
 
 ansi_art='
           ▄▄▄                         ▄▄▄
-         █████   ██    ██  ██████    █████  
-        ██   ██  ██    ██  ██   ██  ██   ██ 
-        ███████  ██    ██  ██████   ███████ 
-        ██   ██  ██    ██  ██   ██  ██   ██ 
-        ██   ██   ██████   ██   ██  ██   ██ 
+         █████   ██    ██  ██████    █████
+        ██   ██  ██    ██  ██   ██  ██   ██
+        ███████  ██    ██  ██████   ███████
+        ██   ██  ██    ██  ██   ██  ██   ██
+        ██   ██   ██████   ██   ██  ██   ██
 
     = Beautiful aesthetics meets powerful dev tools
 '
@@ -26,9 +26,9 @@ echo -e "\nCloning Aura from: https://github.com/${AURA_REPO}.git"
 rm -rf ~/.local/share/aura/
 git clone "https://github.com/${AURA_REPO}.git" ~/.local/share/aura >/dev/null
 
-# Use custom branch if instructed, otherwise default to main
-AURA_REF="${AURA_REF:-main}"
-if [[ $AURA_REF != "main" ]]; then
+# Use custom branch if instructed, otherwise default to -master
+AURA_REF="${AURA_REF:-master}"
+if [[ $AURA_REF != "-master" ]]; then
   echo -e "\e[32mUsing branch: $AURA_REF\e[0m"
   cd ~/.local/share/aura
   git fetch origin "${AURA_REF}" && git checkout "${AURA_REF}"
